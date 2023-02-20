337493f47d651e603dcf03bbb7823d4ad5d020b7
Bruno Camacho <bruno.camacho@umvel.com>
2023-02-20 11:46:06 -0600

    chore: 🔧 upload initial files

---
+++ changelog.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "
+=============================================
+-- Welcome to umvel changelog installer --
+=============================================
+"
+echo "Enter the repository URL: " 
+read repository 
+echo "\n---- Init node ----\n" 
+npm init -y
+echo "\n---- Download dependencies ----\n" 
+npm i --save-dev husky standard-version npm-add-script commitizen cz-conventional-changelog devmoji 
+npx husky install
+npx husky add .husky/prepare-commit-msg "npx devmoji -e --lint"
+npx npmAddScript -k postinstall -v "husky install"
+npx npmAddScript -k release -v "standard-version"
+npx npmAddScript -k release:minor -v "standard-version --release-as minor"
+npx npmAddScript -k release:patch -v "standard-version --release-as patch"
+npx npmAddScript -k release:major -v "standard-version --release-as major"
+echo '{ "path": "cz-conventional-changelog" }'>> .czrc
+echo "{
+  "\"header"\": "\"Changelog"\",
+  "\"types"\": [
+    {"\"type"\": "\"feat"\", "\"section"\": "\"✨ Features"\"},
+    {"\"type"\": "\"fix"\", "\"section"\": "\"🐛 Bug Fixes"\"},
+    {"\"type"\": "\"chore"\", "\"section"\": "\"🚚 Chores"\","\"hidden"\": false},
+    {"\"type"\": "\"docs"\", "\"section"\": "\"📝 Documentation"\","\"hidden"\": false},
+    {"\"type"\": "\"style"\", "\"section"\": "\"💄 Styling"\","\"hidden"\": false},
+    {"\"type"\": "\"refactor"\", "\"section"\": "\"♻️ Code Refactoring"\","\"hidden"\": false},
+    {"\"type"\": "\"perf"\", "\"section"\": "\"⚡️ Performance Improvements"\","\"hidden"\": false},
+    {"\"type"\": "\"test"\", "\"section"\": "\"✅ Testing"\","\"hidden"\": false}
+  ],
+  "\"commitUrlFormat"\": "\"$repository/commit/{{hash}}"\",
+  "\"compareUrlFormat"\": "\"$repository/compare/{{previousTag}}...{{currentTag}}"\",
+  "\"issuePrefixes"\": ["\"#"\"],
+  "\"issueUrlFormat"\": "\"$repository/issues/{{id}}"\",
+  "\"userUrlFormat"\": "\"https://github.com/{{user}}"\"
+}">> .versionrc.json
